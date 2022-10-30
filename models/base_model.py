f3ce81c09de2db8cc333d8c60e56935bf679e64b
Warren Wice <mulubiwarren@gmail.com>
2022-10-30 06:53:34 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-Contains class BaseModel
-"""
+""" Contains class BaseModel """
 
 from datetime import datetime
 import models
-import uuid
+from uuid import uuid4
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -19,16 +17,20 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
-            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
-            models.storage.save()
 
     def __str__(self):
         """String representation of the BaseModel class"""
@@ -38,6 +40,7 @@ class BaseModel:
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
